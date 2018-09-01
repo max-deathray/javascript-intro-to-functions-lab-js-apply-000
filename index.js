8fcf5f546091368f506836e5739a86ad9572091c
McRae Petrey <mpetrey@gmail.com>
2018-09-01 15:14:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function logShout(string) {
   console.log(string.toUpperCase());
 
 }
+
+function logShout(string) {
+  
+  console.log(string.toLowerCase());
+
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too!";
+  }
+}
